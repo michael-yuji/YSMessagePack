5854eb7bc8365523a9902494a0f303e2b27339b9
yuuji <hatsuneyuji@yahoo.co.j>
2016-09-07 21:35:53 -0700

    updated example

--- Example/Sources/main.swift
+++ Example/Sources/main.swift
@@ -36,19 +36,20 @@ class Example {
         // Now we want to pack them and send across
         // the `payload` is a NSData Object and you just
         // send them across
-        let payload = packItems([someString, someInteger, someDouble, someBool, myOwnDataStructure])
+        let payload = pack(items: [someString, someInteger, someDouble, someBool, myOwnDataStructure])
         
         // uncomment to use any of the following 
-        // unpacking methods
-//        example_normal_unpack(payload)
-//        example_async_unpack(payload)
-//        example_async_unapck_for_each(payload)
+        // unpacking methods,
+        
+//        example_normal_unpack(payload: payload)
+//        example_async_unpack(payload: payload)
+//        example_async_unapck_for_each(payload: payload)
         
     }
     
     
     func example_normal_unpack(payload: NSData) {
-        let unpack = try! payload.unpack()
+        let unpack = try! payload.itemsUnpacked()
         
         // Now unpack our packed data
         // This will return an array of `NSData`,
@@ -74,7 +75,7 @@ class Example {
     func example_async_unpack(payload: NSData) {
         // async (non-blocking) unpacking
         // useful when the payload is too big
-        payload.unpackAsync(DISPATCH_QUEUE_PRIORITY_HIGH, competitionHandler: { (unpack) in
+        payload.unpackAsync(priority: DispatchQoS.default, competitionHandler: { (unpack) in
             let strUnpacked = unpack[0].castToString()
             let intUnpacked = unpack[1].castToInt
             let doubleUnpacked = unpack[2].castToDouble
@@ -100,15 +101,15 @@ class Example {
         // unpack the whole payload,
         // This handler will call whenever anyone of 
         // the member finished unpack
-        payload.unpackAsyncForEach(DISPATCH_QUEUE_PRIORITY_HIGH) { (data, type, index) in
+        payload.unpackAsyncForEach(priority: DispatchQoS.default) { (data, type, index) in
             switch type {
-            case .Str_8bit, .Str_16bit, .Str_32bit, .fixstr:
+            case .str8bit, .str16bit, .str32bit, .fixstr:
                 print("Fouond String \(data.castToString()!) at index \(index)")
-            case .fixInt, .fixNegativeInt, .Int8, .Int16, .Int32, .Int64, .UInt8, .UInt16, .UInt32, .UInt64:
+            case .fixInt, .fixNegativeInt, .int8, .int16, .int32, .int64, .uInt8, .uInt16, .uInt32, .uInt64:
                 print("Found Int \(data.castToInt) at index \(index)")
             case .float32, .float64:
                 print("Found Float \(data.castToDouble) at index \(index)")
-            case .Bool:
+            case .bool:
                 print("Found Bool \(data.castToBool) at index \(index)")
             default:
                 break;
@@ -118,3 +119,5 @@ class Example {
 }
 
 Example().run()
+sleep(5) // for async
+
